bd42b6ad698296ab5d66b90de8226d80101d34bf
I-Rinka <I_Rinka@outlook.com>
2021-03-02 20:01:41 +0800

    update c&java version

--- EX1/Haskell/src/Matrix_Multiply.hs
+++ EX1/Haskell/src/Matrix_Multiply.hs
@@ -1,7 +1,7 @@
 import Data.List
 
 multiplyMatrix :: Num x => [[x]] -> [[x]] -> [[x]]
-multiplyMatrix x y = zipWith (zipWith (+)) x y
+multiplyMatrix x y = [ [ sum $ zipWith (*) x_row y_col | y_col <- (transpose y) ] | x_row <- x ]
 
 
 getMatrix n =
@@ -9,8 +9,8 @@ getMatrix n =
   in [rowIndex n k | k <- [1,2..n]]
 
 main = do
-  let matrix_size = 1500::Int
-  let matrix1 = getMatrix matrix_size --得到两个1500*1500的矩阵
+  let matrix_size = 1000::Int
+  let matrix1 = getMatrix matrix_size --得到两个1000*1000的矩阵
   let matrix2 = getMatrix matrix_size
 
   let product = multiplyMatrix matrix1 matrix2
