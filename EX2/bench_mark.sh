7e5a71550cda21f774c47d50f28620cc164cce8e
I-Rinka <I_Rinka@outlook.com>
2021-03-08 21:54:16 +0800

    experiment done

--- EX2/bench_mark.sh
+++ EX2/bench_mark.sh
@@ -14,11 +14,56 @@ echo $((($end_time - $start_time)/10000))
 
 a=0
 
+time_run_10()
+{
 for value in {1..10}
 do
-  a=$(( $a+ $(average_time './src/Matrix_Multiply.out 1000') ))
-  
+  a=$(( $a+ $(average_time './main.out 1000') ))
 done
-
+# echo $a
 echo $(($a/10)).$(($a%10))
+}
 
+
+compile_command='./src/Matrix_Multiply.c ./src/Matrix_Operation.c -o main.out'
+
+
+echo "GCC O0:"
+gcc $compile_command -O0
+echo $(time_run_10)ms
+echo ""
+
+echo "GCC O1:"
+gcc $compile_command -O1
+echo $(time_run_10)ms
+echo ""
+
+echo "GCC O2:"
+gcc $compile_command -O2
+echo $(time_run_10)ms
+echo ""
+
+echo "GCC O3:"
+gcc $compile_command -O3
+echo $(time_run_10)ms
+echo ""
+
+echo "LLVM O0:"
+gcc $compile_command -O0
+echo $(time_run_10)ms
+echo ""
+
+echo "LLVM O1:"
+gcc $compile_command -O1
+echo $(time_run_10)ms
+echo ""
+
+echo "LLVM O2:"
+gcc $compile_command -O2
+echo $(time_run_10)ms
+echo ""
+
+echo "LLVM O3:"
+gcc $compile_command -O3
+echo $(time_run_10)ms
+echo ""
---
+++ EX2/hello/hello_bk.s
@@ -0,0 +1,41 @@
+	.text
+	.file	"hello.c"
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$32, %rsp
+	movl	$0, -4(%rbp)
+	movl	%edi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movabsq	$.L.str, %rdi
+	movb	$0, %al
+	callq	printf
+	xorl	%ecx, %ecx
+	movl	%eax, -20(%rbp)         # 4-byte Spill
+	movl	%ecx, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"hello world\n"
+	.size	.L.str, 13
+
+	.ident	"clang version 10.0.0-4ubuntu1 "
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
+	.addrsig_sym printf
